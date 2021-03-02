c4b170b796002598d37842eac6c382a9efa025f8
Jonny Larsson <jonny@gardnet.nu>
2021-03-02 01:01:08 +0100

    Updated with color,font,border radius support.

--- MudBlazor.ThemeManager/Components/MudThemeManager.razor.cs
+++ MudBlazor.ThemeManager/Components/MudThemeManager.razor.cs
@@ -5,26 +5,74 @@ namespace MudBlazor.ThemeManager
 {
     public partial class MudThemeManager
     {
-        public bool Clipped { get; set; }
-        public bool Dark { get; set; }
-        public bool Rtl { get; set; }
-
-        public string Font { get; set; } = "Montserrat";
+        public string ThemePresets { get; set; } = "Not Implemented";
         public int BorderRadius { get; set; } = 3;
 
         [Parameter] public bool Open { get; set; }
         [Parameter] public EventCallback<bool> OpenChanged { get; set; }
-        [Parameter] public MudTheme Theme { get; set; } = new MudTheme();
-        [Parameter] public EventCallback<MudTheme> ThemeChanged { get; set; }
+        [Parameter] public ThemeManagerTheme Theme { get; set; }
+        [Parameter] public EventCallback<ThemeManagerTheme> ThemeChanged { get; set; }
+
         async Task UpdateOpenValue()
         {
             Open = false;
             await OpenChanged.InvokeAsync(false);
         }
 
-        async Task UpdateTheme()
+        async Task UpdateThemeChanged()
         {
-            await ThemeChanged.InvokeAsync(_currentTheme);
+            await ThemeChanged.InvokeAsync(Theme);
+        }
+
+        protected override void OnInitialized()
+        {
+            _currentTheme = Theme.Theme;
+            _customTheme = Theme.Theme;
+        }
+
+        private void OnDrawerClipMode(DrawerClipMode value)
+        {
+            Theme.DrawerClipMode = value;
+            UpdateThemeChanged();
+        }
+
+        void OnDefaultBorderRadius(int value)
+        {
+            Theme.DefaultBorderRadius = value;
+            var newBorderRadius = _customTheme.LayoutProperties;
+
+            newBorderRadius.DefaultBorderRadius = $"{value}px";
+
+            _customTheme.LayoutProperties = newBorderRadius;
+            _currentTheme = _customTheme;
+
+            UpdateThemeChanged();
+        }
+
+        void OnFontFamily(string value)
+        {
+            Theme.FontFamily = value;
+            var newTypography = _customTheme.Typography;
+
+            newTypography.Body1.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Body2.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Button.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Caption.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Default.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H1.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H2.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H3.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H4.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H5.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.H6.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Overline.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Subtitle1.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+            newTypography.Subtitle2.FontFamily = new[] { value, "Helvetica", "Arial", "sans-serif" };
+
+            _customTheme.Typography = newTypography;
+            _currentTheme = _customTheme;
+
+            UpdateThemeChanged();
         }
 
         void UpdatePalette(ThemeUpdatedValue value)
@@ -116,26 +164,9 @@ namespace MudBlazor.ThemeManager
             _customTheme.Palette = newPalette;
             _currentTheme = _customTheme;
 
-            UpdateTheme();
+            UpdateThemeChanged();
         }
-        protected override void OnInitialized()
-        {
-            _currentTheme = Theme;
-            _customTheme = Theme;
-        }
-
-        private void DarkMode()
-        {
-            //if (_currentTheme == MudBlazorAdminDashboard.DefaultTheme)
-            //{
-            //    _currentTheme = MudBlazorAdminDashboard.DarkTheme;
-            //}
-            //else
-            //{
-            //    _currentTheme = MudBlazorAdminDashboard.DefaultTheme;
-            //}
-        }
-
+        
         public static MudTheme _currentTheme { get; set; }
         public static MudTheme _customTheme { get; set; }
         public static MudTheme _defaultTheme { get; set; } = new MudTheme();
---
+++ MudBlazor.ThemeManager/Models/ThemeManagerTheme.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MudBlazor.ThemeManager
+{
+    public class ThemeManagerTheme
+    {
+        public MudTheme Theme { get; set; } = new MudTheme();
+        public bool RTL { get; set; }
+        public int AppBarElevation { get; set; }
+        public string FontFamily { get; set; }
+        public int DefaultBorderRadius { get; set; }
+        public DrawerClipMode DrawerClipMode { get; set; }
+    }
+}
