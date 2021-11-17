179dafe9342b1a032d59d686cbde094998e91128
Jonny Larsson <jonny@gardnet.nu>
2021-11-17 03:26:58 +0100

    New ThemeManager

---
+++ src/MudBlazor.ThemeManager/Components/ThemeManagerOption.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor.ThemeManager.Components
+{
+    public partial class ThemeManagerOption
+    {
+        /// <summary>
+        /// If specified, title of the themeing option
+        /// </summary>
+        [Parameter] public string Title { set; get; }
+    }
+}
---
+++ src/MudBlazor.ThemeManager/MudThemeManager.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.ThemeManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudBlazor.ThemeManager
+{
+    public partial class MudThemeManager
+    {
+        /// <summary>
+        /// Sets the opened state of Theme Manager.
+        /// </summary>
+        [Parameter] public bool Open { get; set; }
+
+        /// <summary>
+        /// Theme Managers drop shadow.
+        /// </summary>
+        [Parameter] public int Elevation { set; get; } = 1;
+
+        /// <summary>
+        /// Side from which the Theme Manager appears.
+        /// </summary>
+        [Parameter] public Anchor Anchor { get; set; } = Anchor.End;
+
+        /// <summary>
+        /// Disable overlay for Theme Manager.
+        /// </summary>
+        [Parameter] public bool DisableOverlay { get; set; } = true;
+
+        [Parameter] public ThemeManagerTheme Theme { get; set; } = new ThemeManagerTheme();
+
+        private bool _open
+        {
+            get => _open;
+            set => _open = Open;
+        }
+
+        void Test()
+        {
+            Theme.Palette.GrayLight = "#fff";
+            Theme.Typography.H4.FontSize = "2rem";
+            Theme.Palette.Background = "#000";
+        }
+    }
+}
