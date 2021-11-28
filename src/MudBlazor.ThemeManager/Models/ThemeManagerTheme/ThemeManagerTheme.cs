5fcb1476ac033adc14e3c373c192d595d64d06f2
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-11-28 22:03:27 +1100

    Adding theme manager theme.

---
+++ src/MudBlazor.ThemeManager/Models/ThemeManagerTheme/ThemeManagerTheme.cs
@@ -0,0 +1,12 @@
+﻿using MudBlazor.ThemeManager.Enums;
+
+namespace MudBlazor.ThemeManager.Models.ThemeManagerTheme;
+
+internal class ThemeManagerTheme
+{
+    public string PresetThemes { get; set; }
+    public Modes Mode { get; set; }
+    public ThemeManagerThemePalette Palette { get; set; } = new();
+    public int DefaultBorderRadiusAsInt { get; set; }
+    public LayoutProperties LayoutProperties { get; set; }
+}
