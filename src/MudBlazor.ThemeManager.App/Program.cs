ae0b97d80b90dbfeb5aee6d697227b8c8246f399
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-11-28 22:08:09 +1100

    Update demo app.

--- src/MudBlazor.ThemeManager.App/Program.cs
+++ src/MudBlazor.ThemeManager.App/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Net.Http;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 using MudBlazor.ThemeManager.App;
 
@@ -9,5 +13,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
