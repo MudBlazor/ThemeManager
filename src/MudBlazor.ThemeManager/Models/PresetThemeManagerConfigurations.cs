c0fa3f183ca2737921739c32540e04c8dbdcdb20
Alexandre Pereira <alexandre.pereira.b@outlook.com>
2021-11-28 22:03:47 +1100

    Adding preset theme configurations.

---
+++ src/MudBlazor.ThemeManager/Models/PresetThemeManagerConfigurations.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using MudBlazor.ThemeManager.Enums;
+using MudBlazor.ThemeManager.Models.Sections;
+using MudBlazor.Utilities;
+
+namespace MudBlazor.ThemeManager.Models;
+
+public static class PresetThemeManagerConfigurations
+{
+    public static ThemeManagerOptions GetPresetConfigOne(MudTheme? theme = null)
+    {
+        var themeManagerOptions = new ThemeManagerOptions
+        {
+            ShowLayoutSection = true,
+            ShowColorSections = true
+        };
+
+        themeManagerOptions.UpdateColorSection(ColorTitles.Primary, new ColorSectionOptions
+        {
+            Display = true,
+            SelectedColor = Colors.Green.Default
+        });
+
+        themeManagerOptions.UpdateColorSection(ColorTitles.Secondary, new ColorSectionOptions
+        {
+            Display = true,
+            ColorOptions = new List<MudColor>
+            {
+                Colors.Pink.Default,
+                Colors.Amber.Default,
+                Colors.Teal.Lighten1,
+                Colors.Lime.Default
+            },
+            SelectedColor = Colors.Amber.Default
+        });
+
+        themeManagerOptions.UpdateColorSection(ColorTitles.Tertiary, new ColorSectionOptions
+        {
+            Display = true,
+            Variant = ColorSectionVariants.Advanced,
+            SelectedColor = theme is null ? Colors.BlueGrey.Default : theme.Palette.Tertiary.Value
+        });
+
+        return themeManagerOptions;
+    }
+}
