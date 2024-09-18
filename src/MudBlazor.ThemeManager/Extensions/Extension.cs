855c174eef706aeaa77cb5426d218a38681c7de6
Artyom M <artem.melnikov@live.com>
2024-09-18 23:26:25 +0300

    DeepClone: Use sourcegenerator to fix trimming issues

--- src/MudBlazor.ThemeManager/Extensions/Extension.cs
+++ src/MudBlazor.ThemeManager/Extensions/Extension.cs
@@ -6,6 +6,7 @@ namespace MudBlazor.ThemeManager.Extensions;
 internal static class Extension
 {
     private static readonly JsonSerializerOptions JsonOptions = new();
+    private static readonly PaletteSerializerContext PaletteSerializerContext = new();
 
     [RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed.")]
     public static T? DeepClone<T>(this T source)
@@ -18,5 +19,30 @@ internal static class Extension
         }
 
         return default;
+    }
+
+    // TODO: Needs this to be done https://github.com/MudBlazor/MudBlazor/pull/9434
+    //private static readonly ThemeSerializerContext ThemeSerializerContext = new();
+
+    //public static MudTheme? DeepClone(this MudTheme source)
+    //{
+    //    var themeType = typeof(MudTheme);
+    //    var serializeStr = JsonSerializer.Serialize(source, themeType, ThemeSerializerContext);
+    //    var copyObj = (MudTheme?)JsonSerializer.Deserialize(serializeStr, themeType, ThemeSerializerContext);
+
+    //    return copyObj;
+    //}
+
+    public static PaletteDark? DeepClone(this PaletteDark source) => DeepClonePalette(source);
+
+    public static PaletteLight? DeepClone(this PaletteLight source) => DeepClonePalette(source);
+
+    private static T? DeepClonePalette<T>(T source) where T : Palette
+    {
+        var paletteType = typeof(T);
+        var serializeStr = JsonSerializer.Serialize(source, paletteType, PaletteSerializerContext);
+        var copyObj = (T?)JsonSerializer.Deserialize(serializeStr, paletteType, PaletteSerializerContext);
+
+        return copyObj;
     }
 }
---
+++ src/MudBlazor.ThemeManager/Extensions/ThemeSerializerContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MudBlazor.ThemeManager.Extensions;
+
+//[JsonSerializable(typeof(MudTheme))]
+
+//internal sealed partial class ThemeSerializerContext : JsonSerializerContext
+//{
+//}
