ac1415b1514bd2ed5e90586f026e3cc9bd13cab0
Davide @KappesLab <davide.kappelli@gmail.com>
2025-01-06 14:00:34 +0100

    Added new feature: Added switch Theme preset.
    Force update color in Theme Manager Color item on changhe preset.

---
+++ src/MudBlazor.ThemeManager.TestApp/MudThemePresets/CustomThemePreset01.cs
@@ -0,0 +1,19 @@
+﻿namespace MudBlazor.ThemeManager.TestApp.MudThemePresets;
+
+
+public class CustomThemePreset01 : IThemePreset
+{
+    public string NamePreset { get; } = "Arco Service NET";
+
+    public MudTheme Theme { get; } = new()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = Colors.Brown.Darken3,
+            Secondary = Colors.Green.Darken2,
+            AppbarBackground = Colors.Red.Default
+        },
+        PaletteDark = new PaletteDark() { Primary = Colors.Blue.Lighten1 },
+        LayoutProperties = new LayoutProperties() { DrawerWidthLeft = "260px", DrawerWidthRight = "300px" }
+    };
+}
